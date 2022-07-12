9948ff8529b3929f7f9ee0405c5a9dcb13e2c5ee
Razens <razens@gmail.com>
2022-07-12 21:29:41 +0300

    add interview question: convert time

---
+++ interview/convert_time.py
@@ -0,0 +1,80 @@
+"""
+Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+Example
+
+
+Return '12:01:00'.
+
+
+Return '00:01:00'.
+
+Function Description
+
+Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+timeConversion has the following parameter(s):
+
+string s: a time in  hour format
+Returns
+
+string: the time in  hour format
+Input Format
+
+A single string  that represents a time in -hour clock format (i.e.:  or ).
+
+Constraints
+
+All input times are valid
+Sample Input
+
+07:05:45PM
+Sample Output
+
+19:05:45
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    hour = get_hour(s[:-2].split(':')[0], s[-2:])
+    return hour + s[2:-2]
+
+def get_hour(hour, frmt) -> str:
+    if hour=='12' and frmt == 'AM':
+        return '00'
+    elif hour=='12' and frmt == 'PM':
+        return '12'
+    elif frmt == 'AM':
+        return hour
+    elif frmt == 'PM':
+        return str(int(hour) + 12)
+    else:
+        raise Exception('Input error')
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
