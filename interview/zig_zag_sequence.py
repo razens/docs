12261d5d46b5929dfccd2e91be45df8828619291
Razens <razens@gmail.com>
2022-07-14 12:46:01 +0300

    add code zig zag sequence

--- interview/zig_zag_sequence.py
+++ interview/zig_zag_sequence.py
@@ -28,4 +28,56 @@ Constraints
 Output Format
 
 For each test cases, print the elements of the transformed zig zag sequence in a single line.
+
+"""
+
+def findZigZagSequence(a, n):
+    a.sort()
+    mid = int((n + 1)/2)
+    a[mid], a[n-1] = a[n-1], a[mid]
+
+    st = mid + 1
+    ed = n - 1
+    while(st <= ed):
+        a[st], a[ed] = a[ed], a[st]
+        st = st + 1
+        ed = ed + 1
+
+    for i in range (n):
+        if i == n-1:
+            print(a[i])
+        else:
+            print(a[i], end = ' ')
+    return
+
+test_cases = int(input())
+for cs in range (test_cases):
+    n = int(input())
+    a = list(map(int, input().split()))
+    findZigZagSequence(a, n)
+
+
+
+
+"""
+Solution:
+def findZigZagSequence(a, n):
+    a.sort()
+    mid = int(n / 2) # change 1
+    a[mid], a[n-1] = a[n-1], a[mid]
+
+    st = mid + 1
+    ed = n - 2 # change 2
+    while(st <= ed):
+        a[st], a[ed] = a[ed], a[st]
+        st = st + 1
+        ed = ed - 1 # change 3
+
+    for i in range (n):
+        if i == n-1:
+            print(a[i])
+        else:
+            print(a[i], end = ' ')
+    return
+
 """
